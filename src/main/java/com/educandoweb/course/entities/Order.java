467dd641361b34e4a50aaaeca93f8cd4dd1b9a69
Sarah Karoline <sarah_ferreira07@hotmail.com>
2025-11-04 22:27:38 -0300

    Order Status enum

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -24,7 +24,9 @@ public class Order implements Serializable{
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
-    
+
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -33,11 +35,12 @@ public class Order implements Serializable{
         
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
-    }
+}
 
     public Long getId() {
         return id;
@@ -61,6 +64,15 @@ public class Order implements Serializable{
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+       if(orderStatus != null)
+        this.orderStatus = orderStatus.getCode();
     }
 
     @Override
