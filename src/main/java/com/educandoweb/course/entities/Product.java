4e7e54710790a1c4daa5c4ee13e49581f5109821
Sarah Karoline <sarah_ferreira07@hotmail.com>
2025-11-05 10:47:49 -0300

    Many-to-many associaton with JoinTable

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -3,11 +3,15 @@ package com.educandoweb.course.entities;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -24,7 +28,10 @@ private static final long serialVersionUID = 1L;
     private Double price;
     private String imgUrl;
 
-    @Transient
+    @ManyToMany
+    @JoinTable(name = "tb_product_category", 
+    joinColumns = @jakarta.persistence.JoinColumn(name = "product_id"), 
+    inverseJoinColumns = @jakarta.persistence.JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
     public Product() {
