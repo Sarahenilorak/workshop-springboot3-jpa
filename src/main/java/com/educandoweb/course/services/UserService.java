ec3bde051cf36ebf3c8d0e7fa7edfa5608660b78
Sarah Karoline <sarah_ferreira07@hotmail.com>
2025-11-06 09:02:10 -0300

    Exception handling - Update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -37,11 +39,14 @@ public class UserService {
         }
     }       
     public User update(Long id, User obj) {
+        try{
         User entity = repository.getReferenceById(id);
         updateData(entity, obj);
         return repository.save(entity);
-    }
-
+        } catch (EntityNotFoundException e){
+           throw new ResourceNotFoundException(id); 
+        }
+}
     private void updateData(User entity, User obj) {
         entity.setName(obj.getName());
         entity.setEmail(obj.getEmail());
